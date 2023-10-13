5f570635779410206ccd95c6c723e3b43c345208
ralphman1 <roottdeve10per0@gmail.com>
2023-10-13 16:24:00 +0200

    bugs fixed

--- store/feature/authSlice.js
+++ store/feature/authSlice.js
@@ -79,26 +79,6 @@ export const verifyUser = createAsyncThunk(
     }
   }
 );
- 
-export const verifyUserEmail = createAsyncThunk(
-  'auth/verifyUserEmail',
-
-  async ({ prams }, thunkAPI) => {
-    try {
-      const response = await tradly.user.forgotPassword({ data: prams });
-      const { data } = await response;
-      if (!response.error) {
-        return data;
-      } else {
-        const { error } = await response;
-        return error;
-      }
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data);
-    }
-  }
-);
- 
 
 export const authSlice = createSlice({
   name: 'auth',
@@ -255,28 +235,6 @@ export const authSlice = createSlice({
       state.errorMessage = '';
     },
     [verifyUser.rejected]: (state, { payload }) => {
-      state.isFetching = false;
-      state.isError = true;
-      state.errorMessage = payload?.message;
-    },
-    [verifyUserEmail.fulfilled]: (state, { payload }) => {
-      if (payload.code) {
-        state.isFetching = false;
-        state.isError = true;
-        state.isSuccess = false;
-        state.errorMessage = payload?.message;
-      } else {
-        state.isError = false;
-        state.isFetching = false;
-        state.isSuccess = true;
-      }
-    },
-    [verifyUserEmail.pending]: (state) => {
-      state.isFetching = true;
-      state.isError = false;
-      state.errorMessage = '';
-    },
-    [verifyUserEmail.rejected]: (state, { payload }) => {
       state.isFetching = false;
       state.isError = true;
       state.errorMessage = payload?.message;
