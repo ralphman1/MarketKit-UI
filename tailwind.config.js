02bd068229ffaae4a3731de92bb581a5926d6cbb
ralphman1 <roottdeve10per0@gmail.com>
2023-10-28 13:26:06 +0200

    poc udating

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './themes/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
+  corePlugins: {
+    blur: false,
+  },
   theme: {
     extend: {
       fontSize: {
@@ -32,6 +39,9 @@ module.exports = {
       },
       borderRadius: {
         'c-48': '48px',
+      },
+      outline: {
+        blue: '2px solid var( --primary_color)',
       },
     },
     screens: {
